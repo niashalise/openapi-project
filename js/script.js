3a4df6a7dd55becd683a6043c3e6768151b74da7
Nia <shalise.nia@gmail.com>
2024-09-10 20:51:31 -0400

    changed focus of page and added styles and js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
-//fetch
-const apiKey = "live_8MHa7O2P9YSuZSukChUap1VVlx7Fv5ffP76hQbmAbYgLz2O9hW535q9JMwUVYS9w"
+const mango = document.getElementById('mango');
+const banana = document.getElementById('banana');
+const apple = document.getElementById('apple');
+const pineapple = document.getElementById('pineapple');
+const orange = document.getElementById('orange');
+const kiwi = document.getElementById('kiwi');
+const strawberry = document.getElementById('strawberry');
+const grape = document.getElementById('grape');
+const lychee = document.getElementById('lychee');
+const watermelon = document.getElementById('watermelon');
+const papaya = document.getElementById('papaya');
+const lime = document.getElementById('lime');
 
-fetch('https://api.thedogapi.com/', {
-    headers: {'x-api-key': apiKey}
+const fruits = document.querySelector('.fruits')
+//create event listener that will give a description of each fruit when the fruit is clicked
+fruits.addEventListener("click", function (e) {
+    console.log(mango);
+    console.log(apple);
 })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Error: ", response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error("Error: ", error))
+
+
+
+//fetch
+
+// REMOVED
+
+// fetch("https://wger.de/api/v2/workout/", {
+//     headers: {'x-api-key': apiKey}
+// })
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error("Error: ", response.statusText);
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error("Error: ", error))
 
